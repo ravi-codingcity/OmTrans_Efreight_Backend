f5067413b36404e725c1bf3eb9683a83ba13c227
ravicodingcity <ravich27@outlook.com>
2025-12-27 17:25:51 +0530

    CORS with specific allowed origins including efreightpro.in

--- src/server.js
+++ src/server.js
@@ -8,11 +8,16 @@ dotenv.config();
 // Initialize express app
 const app = express();
 
-// Simple CORS - set headers on ALL responses
+// CORS - Allow specific origin
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = ['https://efreightpro.in', 'http://localhost:3000', 'http://localhost:5173'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
