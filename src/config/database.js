d7b14bcf2193fec797909280e247e0c426c4040e
Ravi Chaudhary <ravi807609@gmail.com>
2026-03-03 11:32:54 +0530

    Fix: Use Google DNS for MongoDB Atlas SRV resolution

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Use Google DNS to resolve MongoDB Atlas SRV records
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
   try {
