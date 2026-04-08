bc0b65db92f52d8c90b1787e6d2a7064edfc7040
Ravi Chaudhary <ravi807609@gmail.com>
2026-04-08 12:36:39 +0530

    CORS error

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 // CORS - Allow all origins
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
